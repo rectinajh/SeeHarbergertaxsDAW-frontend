e8078acd6ae686948e830e60feb883c718aa2ab8
“rectinajh” <rectinajh@163.com>
2025-03-22 16:28:41 +0800

    update

--- components/common/EmptyText.tsx
+++ components/common/EmptyText.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
-
-export const emptyText = <p className="h-[calc(100vh-180px)] max-w-[203] m-auto flex items-center justify-center">暂无申请记录，快去首页购买广告进行配置吧</p>
+// 使用函数组件形式定义，而不是直接JSX赋值
+const EmptyTextComponent = () => {
+    return (
+      <p className="h-[calc(100vh-180px)] max-w-[203] m-auto flex items-center justify-center">
+        暂无申请记录，快去首页购买广告进行配置吧
+      </p>
+    );
+  };
+  
+  // 导出组件实例
+  export const emptyText = <EmptyTextComponent />;
+  
---
+++ components/common/IndexMobie.tsx
@@ -0,0 +1,4 @@
+import { Props } from "react-infinite-scroll-component";
+
+export default function IndexMobie({ pageChange, total, data, handleDetail, currTab, setCurrTab }: Props) { 
+}
